5884e3047eb243698f9f7e119fed2cf101d04830
Won Lee <wonseob2207@gmail.com>
2025-01-31 13:44:29 -0600

    finished anonymous functions php

--- anonymous_functions.php
+++ anonymous_functions.php
@@ -1,4 +1,25 @@
 <?php
-    echo "Hello World";
-    // this is a comment
+    
+$greet = function ($name){
+    return "Hello, $name\n";
+};
+
+echo $greet("David");
+
+$numbers = [1,2,3];
+$squared_numbers = array_map(function ($n){
+    return $n * $n;
+}, $numbers);
+
+var_dump($numbers, $squared_numbers);
+
+$message =  "Bye";
+$greet2 = function ($name) use ($message){
+    $message = $message . "!";
+    return "$message, $name\n";
+};
+
+echo $greet2("David");
+echo $message . "\n";
+
 ?>
